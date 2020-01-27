85fc0679d9c209997046464af9146da482707a36
Denis Pavlovic <denis.pavlovic50@e-uvt.ro>
2020-01-27 20:23:36 +0100

    Added action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import {
-    Text
+    Text,
+    TouchableWithoutFeedback,
+    View
 } from 'react-native';
+import { connect } from 'react-redux';
 import { 
     CardSection 
 } from './common';
+// In the actions file, you can have many actions creator, * stands for selecting all as actions, like foreach
+import * as actions from '../actions';
 
 class ListItem extends Component {
 
+    renderDescription() {
+
+        const { library, expanded } = this.props;
+        if(expanded)
+        {
+            return (
+                <Text>{library.item.description}</Text>
+                
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
+        const {
+            id,
+            title
+        } = this.props.library.item;
+
         return (
-            <CardSection >
-                <Text style={titleStyle}>
-                    {this.props.library.item.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -27,4 +56,11 @@ const styles = {
     }
 }
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
